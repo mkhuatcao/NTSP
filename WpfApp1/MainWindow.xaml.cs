8c8f1e93fead5753f6aa72345688a3cbd5e73ba7
maciej.khuatcao <maciej.khuatcao@gmail.com>
2019-05-03 15:18:02 +0200

    Initial

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using TSP;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TspResult bestTspRes = null;
+        private List<Location> parent1;
+        private List<Location> parent2;
+        private Stopwatch sw;
+        private Task[] tasksArray;
+        private int phase1Time;
+        private int phase2Time;
+        private Task Refresher;
+        private bool needsRefresh = false;
+        private int ThreadId;
+        private bool isWorking = false;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyCanvas.SizeChanged += MyCanvas_SizeChanged;
+            Phase1TimeControl.ValueChanged += Phase1ControlChanged;
+            Phase2TimeControl.ValueChanged += Phase2ControlChanged;
+        }
+
+        private void Phase1ControlChanged(object sender, System.EventArgs e)
+        {
+            phase1Time = (int)Phase1TimeControl.Value;
+        }
+
+        private void Phase2ControlChanged(object sender, System.EventArgs e)
+        {
+            phase2Time = (int)Phase2TimeControl.Value;
+        }
+
+        private void MyCanvas_SizeChanged(object sender, System.EventArgs e)
+        {
+            List<Location> tour = null;
+            if (bestTspRes != null)
+            {
+                tour = new List<Location>(bestTspRes.BestTour);
+            }
+            if (tour != null)
+            {
+                draw_graph(tour);
+            }
+        }
+
+        private void btn_chooseFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            if (result == true)
+            {
+                FileNameTextBox.Text = openFileDlg.FileName;
+            }
+        }
+
+        private Ellipse DotFactory(Point p)
+        {
+            double dotSize = 10;
+            Ellipse dot = new Ellipse();
+            dot.Stroke = new SolidColorBrush(Colors.Red);
+            dot.StrokeThickness = 3;
+            dot.Height = dotSize;
+            dot.Width = dotSize;
+            dot.Fill = new SolidColorBrush(Colors.Red);
+            dot.Margin = new Thickness(p.X - dotSize/2, p.Y - dotSize/2, 0, 0);
+            return dot;
+        }
+
+        public void draw_graph(List<Location> tour)
+        {
+            MyCanvas.Children.Clear();
+            double margin = 0.3 * 1000;
+            double currentWidth = MyCanvas.ActualWidth;
+            double currentHeight = MyCanvas.ActualHeight;
+
+            double max_X = tour.Max(l => l.X);
+            double max_Y = tour.Max(l => l.Y);
+
+            double min_X = tour.Min(l => l.X);
+            double min_Y = tour.Min(l => l.Y);
+
+            PointCollection points = new PointCollection();
+
+            foreach (var location in tour)
+            {
+                double newX = (location.X - min_X + margin / 2) * (currentWidth / (max_X - min_X + margin));
+                double newY = (location.Y - min_Y + margin / 2) * (currentHeight / (max_Y - min_Y + margin));
+
+                Point p = new Point(newX, newY);
+                points.Add(p);
+            }
+            points.Add(points[0]);
+
+            Polyline mypolyline = new Polyline();
+            mypolyline.Points = points;
+            mypolyline.Stroke = new SolidColorBrush(Colors.Black);
+            mypolyline.StrokeThickness = 2;
+            MyCanvas.Children.Add(mypolyline);
+
+            foreach (var point in points)
+            {
+                MyCanvas.Children.Add(DotFactory(point));
+            }
+
+            OptimalTourTable.ItemsSource = tour;
+        }
+
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            //GUI 
+            BtnStart.Visibility = Visibility.Collapsed;
+            BtnExit.Visibility = Visibility.Hidden;
+            BtnChooseFile.IsEnabled = false;
+            LblStatus.Content = "Processing";
+            TasksCountControl.IsEnabled = false;
+
+            //variables
+            phase1Time = (int)Phase1TimeControl.Value;
+            phase2Time = (int)Phase2TimeControl.Value;
+            isWorking = true;
+
+            //Get locations from file
+            bestTspRes = new TspResult();
+            var dataModel = new DataModel(FileNameTextBox.Text);
+
+            //Start timer
+            sw = new Stopwatch();
+            sw.Start();
+
+            //Set initial values
+            bestTspRes.Distance = Double.MaxValue;
+            bestTspRes.SolutionCount = 1;
+            
+            //Init parents
+            parent1 = new List<Location>(dataModel.Data);
+            parent2 = new List<Location>(dataModel.Data);
+
+            //Shuffle parents
+            Task taskA = Task.Run(() => parent1.Shuffle());
+            Task taskB = Task.Run(() => parent2.Shuffle());
+            taskA.Wait();
+            taskB.Wait();
+            //Set one parent as best
+            checkAndHandleIfBetter(parent1);
+            checkAndHandleIfBetter(parent2);
+
+            tasksArray = new Task[(int)TasksCountControl.Value];
+            for (int i = 0; i < tasksArray.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    tasksArray[i] = Task.Run(() => phase1());
+                }
+                else
+                {
+                    tasksArray[i] = Task.Run(() => phase2());
+                }
+
+            }
+            Refresher = Task.Run(() => refresh());
+            BtnStop.Visibility = Visibility.Visible;
+        }
+
+        private void phase1()
+        {
+            List<Location> list1;
+            List<Location> list2;
+            while (sw.Elapsed.TotalSeconds < phase1Time)
+            {
+                lock (bestTspRes)
+                {
+                    bestTspRes.SolutionCount++;
+                }
+                lock (parent1) lock (parent2)
+                {
+                    list1 = new List<Location>(parent1);
+                    list2 = new List<Location>(parent2);
+                }
+
+                List<Location> newlist1 = Utils.PMX(list1, list2);
+                List<Location> newlist2 = Utils.PMX(list2, list1);
+                
+                checkAndHandleIfBetter(newlist1);
+                checkAndHandleIfBetter(newlist2);
+                lock (parent1) lock (parent2)
+                {
+                    parent1 = new List<Location>(newlist1);
+                    parent2 = new List<Location>(newlist2);
+                }
+            }
+        }
+
+        private void phase2()
+        {
+            List<Location> list1;
+            List<Location> list2;
+            while (sw.Elapsed.TotalSeconds < phase2Time)
+            {
+                lock (bestTspRes)
+                {
+                    bestTspRes.SolutionCount++;
+                }
+                lock (parent1) lock (parent2)
+                {
+                    list1 = new List<Location>(parent1);
+                    list2 = new List<Location>(parent2);
+                }
+                list1.SwapEdges();
+                checkAndHandleIfBetter(list1);
+                list2.SwapEdges();
+                checkAndHandleIfBetter(list2);
+                lock (parent1) lock (parent2)
+                {
+                    parent1 = new List<Location>(list1);
+                    parent2 = new List<Location>(list2);
+                }
+            }
+        }
+
+        private void refresh()
+        {
+            while (isWorking)
+            {
+                List<Location> tour;
+                lock (bestTspRes)
+                {
+                    Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        LblBestResult.Content = bestTspRes.Distance;
+                        LblSolutionCount.Content = bestTspRes.SolutionCount;
+                        LblThreadNumber.Content = ThreadId;
+                    }));
+                    if (needsRefresh)
+                    {
+                        needsRefresh = false;
+                        tour = new List<Location>(bestTspRes.BestTour);
+                        Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            draw_graph(tour);
+                        }));
+                    }
+                }
+                Thread.Sleep(500);
+                if (sw.Elapsed.TotalSeconds > phase2Time && sw.Elapsed.TotalSeconds > phase1Time)
+                    isWorking = false;
+            }
+            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                btn_stop_Click(null, null);
+            }));
+        }
+
+        private void checkAndHandleIfBetter(List<Location> tour)
+        {
+            bool result = false;
+            double distance = Utils.DistanceSum(tour);
+            lock (bestTspRes)
+            {
+                if (bestTspRes.Distance > distance)
+                {
+                    bestTspRes.Distance = distance;
+                    bestTspRes.BestTour = new List<Location>(tour);
+                    result = true;
+                    needsRefresh = true;
+                    ThreadId = Thread.CurrentThread.ManagedThreadId;
+                }
+            }
+        }
+
+        private void btn_end_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            phase1Time = 0;
+            phase2Time = 0;
+            isWorking = false;
+
+            BtnStop.Visibility = Visibility.Collapsed;
+            BtnExit.Visibility = Visibility.Visible;
+            BtnChooseFile.IsEnabled = true;
+            LblStatus.Content = "Ready";
+            TasksCountControl.IsEnabled = true;
+
+            BtnStart.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ WpfApp1/Utils.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP
+{
+    public static class Utils
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            RNGCryptoServiceProvider r = new RNGCryptoServiceProvider();
+            int count = list.Count;
+            while (count > 0)
+            {
+                byte[] buffer = new byte[4];
+                r.GetBytes(buffer);
+
+                int idx = ((BitConverter.ToInt32(buffer, 0) & 0x7FFFFFFF) % count);
+                T value = list[idx];
+                list[idx] = list[--count];
+                list[count] = value;
+            }
+        }
+
+        public static List<Location> PMX(List<Location> parent1, List<Location> parent2)
+        {
+            int count = parent1.Count;
+            List<Location> child_list = new List<Location>();
+
+            for (int i = 0; i < count; i++)
+            {
+                child_list.Add(new Location());
+            }
+            foreach (var location in child_list)
+            {
+                location.Id = -1;
+            }
+
+            RNGCryptoServiceProvider r = new RNGCryptoServiceProvider();
+            byte[] buffer = new byte[4];
+            r.GetBytes(buffer);
+
+            int min = 4;
+            int init_size = ((BitConverter.ToInt32(buffer, 0) & 0x7FFFFFFF) % (count -min)) + min;
+
+            int start_idx = ((BitConverter.ToInt32(buffer, 0) & 0x7FFFFFFF) % (count-init_size));
+            
+            List<int> parent1SubString = new List<int>(init_size);
+            List<int> parent2SubString = new List<int>(init_size);
+
+            //copy initial substring and save id of parent1 and parent2 substring
+            for (int i = start_idx; i < start_idx+init_size; i++)
+            {
+                child_list[i].Id = parent1[i].Id;
+                child_list[i].X = parent1[i].X;
+                child_list[i].Y = parent1[i].Y;
+
+                parent1SubString.Add(parent1[i].Id);
+                parent2SubString.Add(parent2[i].Id);
+            }
+
+            bool found = false;
+            //find place for elements from parent2 substring
+            for (int i = 0; i < init_size; i++)
+            {
+                if(!parent1SubString.Contains(parent2SubString[i]))
+                {
+                    found = false;
+                    int index = i;
+                    while (!found)
+                    {
+                        int tmp2 = parent2SubString.IndexOf(parent1SubString[index]);
+                        if (tmp2 == -1)
+                        {
+                            found = true;
+                        }
+                        else
+                        {
+                            index = tmp2;
+                        }
+                    }
+                    index = parent2.FindIndex(l => l.Id == parent1SubString[index]);
+                    child_list[index].Id = parent2[start_idx + i].Id;
+                    child_list[index].X = parent2[start_idx + i].X;
+                    child_list[index].Y = parent2[start_idx + i].Y;
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                if (child_list[i].Id == -1)
+                {
+                    child_list[i].Id = parent2[i].Id;
+                    child_list[i].X = parent2[i].X;
+                    child_list[i].Y = parent2[i].Y;
+                }
+            }
+
+            return child_list;
+        }
+
+        public static void SwapEdges(this IList<Location> list)
+        {
+            if (list == null) return;
+            List<Edge> edges = list.ConvertToEdgeList();
+            RNGCryptoServiceProvider r = new RNGCryptoServiceProvider();
+
+            int count = edges.Count;
+            byte[] buffer = new byte[8];
+            r.GetBytes(buffer);
+            int idx = ((BitConverter.ToInt32(buffer, 0) & 0x7FFFFFFF) % count);
+            int idx2 = ((BitConverter.ToInt32(buffer, 4) & 0x7FFFFFFF) % count);
+
+            var edge1 = edges[idx];
+            var edge2 = edges[idx2];
+
+            while (!ValidateEdges(edge1, edge2))
+            {
+                r.GetBytes(buffer);
+                idx2 = ((BitConverter.ToInt32(buffer, 0) & 0x7FFFFFFF) % count);
+                edge2 = edges[idx2];
+            }
+
+            var i = list.IndexOf(edge1.B);
+            var j = list.IndexOf(edge2.B);
+            var start = Math.Min(i, j);
+            var end = (start == i) ? j : i;
+            list.ReverseSection(start, end);
+        }
+
+        // start - inclusive
+        // end - exclusive
+        public static void ReverseSection<T>(this IList<T> list, int start, int end)
+        {
+            int count = (end - start) / 2;
+            for (int i = start; i < end; i++)
+            {
+                if (count == 0) break;
+                var idx = end - (i - start + 1);
+                T value = list[i];
+                list[i] = list[idx];
+                list[idx] = value;
+                count--;
+            }
+        }
+
+        private static bool ValidateEdges(Edge e1, Edge e2)
+        {
+            return (!e1.Equals(e2) && !ShareVerticle(e1, e2));
+        }
+
+        private static bool ShareVerticle(Edge e1, Edge e2)
+        {
+            return (e1.A == e2.A || e1.A == e2.B || e1.B == e2.A || e1.B == e2.B);
+        }
+
+        public static List<Edge> ConvertToEdgeList(this IList<Location> list)
+        {
+            List<Edge> edges = new List<Edge>();
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                edges.Add(new Edge()
+                {
+                    A = list[i],
+                    B = list[i + 1]
+                });
+
+            }
+            edges.Add(new Edge()
+            {
+                A = list[list.Count - 1],
+                B = list[0]
+            });
+
+            return edges;
+        }
+
+
+        public static double Distance(Location a, Location b)
+        {
+            double dX = a.X - b.X;
+            double dY = a.Y - b.Y;
+            return Math.Sqrt(Math.Pow(dX, 2) + Math.Pow(dY, 2));
+        }
+
+        public static double DistanceSum(List<Location> list)
+        {
+            double sum = 0;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                sum += Distance(list[i], list[i + 1]);
+            }
+            sum += Distance(list[list.Count - 1], list[0]);
+
+            return sum;
+        }
+
+    }
+
+    public class Edge : IEquatable<Edge>
+    {
+        public Location A { get; set; }
+        public Location B { get; set; }
+
+        public bool Equals(Edge other)
+        {
+            return (this.A == other.A && this.B == other.B);
+        }
+    }
+
+    public class TspResult
+    {
+        public List<Location> BestTour { get; set; }
+        public double Distance { get; set; }
+        public double SolutionCount { get; set; }
+    }
+}
